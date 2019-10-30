a890353e744a2edca1b8118a0cb1d95784d7cb95
sibenshtern <sibenshtern@gmail.com>
2019-10-30 23:38:12 +0300

    add new function

--- functions.py
+++ functions.py
@@ -1,6 +1,18 @@
 import os
+import sqlite3
 
 
 def get_path(directory, file):
     return os.path.join(directory, file)
 
+
+def get_image(image):
+    return get_path('images', image)
+
+
+def get_homepage():
+    con = sqlite3.connect('data.sql')
+    cur = con.cursor()
+    result = cur.execute("SELECT value FROM settings "
+                         "WHERE name='homepage'").fetchone()
+    return result[0]
